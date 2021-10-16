4221b39ab0faad4bee03aac762f80f5cee83c13a
cranade <chanderranade@gmail.com>
2021-10-16 15:52:53 -0400

    Challenge

---
+++ main.py
@@ -0,0 +1,23 @@
+import flask
+from flask import Flask 
+
+import numpy
+app 
+ 
+
+
+
+a = 10
+b = 12
+print(a+b)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+app = Flask(__name__)
+@app.route('/')
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+export FLASK_APP=main.pypython
+set FLASK_APP=main.py
+flask run
